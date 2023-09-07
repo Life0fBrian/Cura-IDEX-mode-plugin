d976f41687e33ec5c9bd879f8b54da419973ed43
Brian <84620081+Life0fBrian@users.noreply.github.com>
2023-09-07 17:01:23 +0200

    Add files via upload

--- IDEXModePlugin.py
+++ IDEXModePlugin.py
@@ -13,6 +13,16 @@ from UM.Settings.ContainerRegistry import ContainerRegistry
 from UM.Logger import Logger
 from cura import CuraActions
 from typing import Dict, List, Any
+from copy import deepcopy
+
+from UM.Scene.Platform import Platform
+from UM.Scene.SceneNode import SceneNode
+from UM.Math.Vector import Vector
+from cura.CuraApplication import CuraApplication
+from UM.Scene.Iterator.BreadthFirstIterator import BreadthFirstIterator
+
+from UM.Message import Message
+
 
 class IDEXModePlugin(Extension):
     
@@ -20,10 +30,12 @@ class IDEXModePlugin(Extension):
         super().__init__()    
     
         self._application = Application.getInstance()
-        self._curaActions = CuraActions.CuraActions()
                 
         self._i18n_catalog = None
         self._build_width = 0   #build plate width initilization
+        self._platform_offset = [0, 0, 0]
+        self._platform_new_offset = [0, 0, 0]
+        self._root_node = None
         
         settings_definition_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "idex_mode_settings.def.json")
         try:
@@ -38,7 +50,8 @@ class IDEXModePlugin(Extension):
         self._application.globalContainerStackChanged.connect(self._onGlobalContainerStackChanged)
         self._onGlobalContainerStackChanged()        
         ContainerRegistry.getInstance().containerLoadComplete.connect(self._onContainerLoadComplete)
-    
+        
+    #get the printers bed width (x axis) from the definitions
     def _getXWidth(self):
         machine_manager = self._application.getMachineManager()
         global_stack = machine_manager.activeMachine
@@ -50,6 +63,29 @@ class IDEXModePlugin(Extension):
             x_width = self._global_container_stack.getProperty("machine_width", "default_value")
             
         return x_width
+
+    #if a custom build plate model (STL or 3MF) is present get its offset
+    def _getPlatformOffset(self):
+        platform_offset = [0,0,0] #initialize variable
+        self._global_container_stack = Application.getInstance().getGlobalContainerStack()
+        if self._global_container_stack:
+            container = self._global_container_stack.findContainer({ "platform": "*" })
+            if container:
+                platform_offset = container.getMetaDataEntry("platform_offset")
+                if not platform_offset:
+                    platform_offset = [0,0,0]
+
+            return platform_offset
+        else:
+            return None
+
+    def _addPlatformXOffset(self):
+        offset = deepcopy(self._platform_offset)
+        x_offset = offset[0]
+       
+        offset[0] = float(x_offset) + float(self._build_width / 4)
+    
+        return offset
 
     def _onContainerLoadComplete(self, container_id: str) -> None:
         if not ContainerRegistry.getInstance().isLoaded(container_id):
@@ -112,52 +148,75 @@ class IDEXModePlugin(Extension):
         self._global_container_stack = self._application.getGlobalContainerStack()
         if self._global_container_stack:
             self._build_width = self._getXWidth()
-            idex_mode = self._global_container_stack.getProperty("idex_mode", "value")
+            self._platform_offset = self._getPlatformOffset()
+            self._platform_new_offset = self._addPlatformXOffset()
 
             self._global_container_stack.propertyChanged.connect(self._onPropertyChanged)
             
             #calling _onPropertyChanged for initialization
+            self._onPropertyChanged("machine_width", "value")
             self._onPropertyChanged("idex_mode", "value")
-            self._onPropertyChanged("idex_change_width", "value")
 
     def _onPropertyChanged(self, key: str, property_name: str) -> None:
         extruder_count = len(self._global_container_stack.extruderList)
         
         if extruder_count > 1:
+            self._root_node = CuraApplication.getInstance().getController().getScene().getRoot()
             idex_mode = self._global_container_stack.getProperty("idex_mode", "value")
             adapt_width = self._global_container_stack.getProperty("idex_change_width", "value")
             
+            offset = self._platform_offset
+            offset_adapt = self._platform_new_offset
+
             if key == "idex_change_width" and property_name == "value":
                 if adapt_width:
                     self._global_container_stack.setProperty("machine_width", "value", self._build_width / 2)
                 else:
-                    self._global_container_stack.setProperty("machine_width", "value", self._build_width)        
+                    self._global_container_stack.setProperty("machine_width", "value", self._build_width)
         
             elif key == "machine_width" and property_name == "value":
-                self._build_width = self._build_width = self._getXWidth()
-                self._onPropertyChanged("idex_mode", "value")            
-                
+                self._build_width = self._getXWidth()
+                self._platform_offset = self._getPlatformOffset()
+                self._platform_new_offset = self._addPlatformXOffset()
+                self._onPropertyChanged("idex_mode", "value")
+                                
             elif key == "idex_mode" and property_name == "value":
                 extruder_t0 = self._global_container_stack.extruderList[0]
                 extruder_t1 = self._global_container_stack.extruderList[1]
-
+                                
                 if idex_mode in ["mirror", "copy"]:
                     self._application.getMachineManager().setExtruderEnabled(0, True)
                     self._application.getMachineManager().setExtruderEnabled(1, False)
+                    
                     if adapt_width:
                         self._global_container_stack.setProperty("machine_width", "value", self._build_width / 2)
+                        for node in BreadthFirstIterator(self._root_node):
+                            #message = Message("", title = "")
+                            #message.show()
+
+                            if isinstance(node, Platform):
+                                node.setPosition(Vector(offset_adapt[0], offset_adapt[1], offset_adapt[2], 3))
                     else:
                         self._global_container_stack.setProperty("machine_width", "value", self._build_width)
+  
+                        for node in BreadthFirstIterator(self._root_node):
+                            if isinstance(node, Platform):
+                                node.setPosition(Vector(offset[0], offset[1], offset[2]))      
 
                 elif idex_mode == "idex":
                     self._application.getMachineManager().setExtruderEnabled(0, True)
                     self._application.getMachineManager().setExtruderEnabled(1, True)
                     self._global_container_stack.setProperty("machine_width", "value", self._build_width)
-                 
+                    for node in BreadthFirstIterator(self._root_node):
+                        if isinstance(node, Platform):
+                            node.setPosition(Vector(offset[0], offset[1], offset[2]))
+                            
                 extruder_t0.enabledChanged.connect(self._onEnabledChangedT0)
                 extruder_t1.enabledChanged.connect(self._onEnabledChangedT1)
+                
         else:   #for single extruder printers set IDEX mode to always idex
-            self._global_container_stack.setProperty("idex_mode", "value", "idex")
+            if key == "idex_mode" and property_name == "value":
+                self._global_container_stack.setProperty("idex_mode", "value", "idex")
 
     def _onEnabledChangedT0(self):
         idex_mode = self._global_container_stack.getProperty("idex_mode", "value")
